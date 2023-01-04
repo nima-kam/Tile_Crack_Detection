a03ae49833959f7263b2abd07ecff80a8ea035d6
samanmr <saman.raouf@gmail.com>
2023-01-04 22:50:33 +0330

    initial instructions design pattern

---
+++ main.py
@@ -0,0 +1,18 @@
+def crop():
+    return None
+
+def histogram_matching():
+    return None
+
+def rotation_matching():
+    return None
+
+def binary_threshold():
+    return None
+
+def predict():
+    crop()
+    histogram_matching()
+    rotation_matching()
+    binary_threshold()
+    return None
