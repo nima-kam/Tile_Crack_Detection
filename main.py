23997a593dca77c9606eee91d8e9e70c3c19015d
Nima <56191102+nima-kam@users.noreply.github.com>
2023-01-07 18:00:30 +0330

    main file prototype improved

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-def crop():
+constants ={
+    "image_size":(1000,1000),
+
+}
+
+def crop(image, width=None, height=None):
     return None
 
-def histogram_matching():
+def histogram_matching(image,pattern):
     return None
 
-def rotation_matching():
+def rotation_matching(image,pattern):
     return None
 
-def binary_threshold():
+def binary_threshold(image):
     return None
 
-def predict():
-    crop()
-    histogram_matching()
-    rotation_matching()
-    binary_threshold()
+
+def predict(img, pattern):
+    """
+    :params: img: input RGB Tile image
+    :params: pattern: input RGB Tile pattern
+    """
+
+    crop_img=crop(image=img)
+    matched_img=histogram_matching(image=crop_img,pattern=pattern)
+    rotated_img=rotation_matching(image=crop_img,pattern=pattern)
+    bi_img=binary_threshold(rotated_img)
+    
     return None
